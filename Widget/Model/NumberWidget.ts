310abc292f5540f4060c955c21e4af26fc27d20b
Tom Schwiha <tom@schwiha.de>
2023-06-20 12:02:53 +0200

    Initial commit

---
+++ Widget/Model/NumberWidget.ts
@@ -0,0 +1,7 @@
+
+export default class NumberWidget
+{
+    component: string;
+    label: string;
+    value: string;
+}
---
+++ Widget/WidgetRegistry.ts
@@ -0,0 +1,9 @@
+import { Registry } from "@enhavo/core";
+import WidgetFactoryInterface from "./WidgetFactoryInterface";
+
+export default class WidgetRegistry extends Registry
+{
+    getFactory(name: string): WidgetFactoryInterface {
+        return <WidgetFactoryInterface>super.getFactory(name);
+    }
+}
